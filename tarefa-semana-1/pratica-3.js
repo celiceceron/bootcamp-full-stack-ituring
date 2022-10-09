91ef7f896a7353c4c4cb1b0eff56ddcf60439cab
celiceceron <celice.cer@gmail.com>
2022-10-09 20:00:42 +0200

    operadores de comparacao

---
+++ tarefa-semana-1/pratica-3.js
@@ -0,0 +1,28 @@
+const nome = "celice" 
+const ehTexto = typeof(nome) == "string"
+
+
+const email = "celice.cer@gmail.com"
+const ehTextoo = typeof(email) == "string"
+
+let profissao = "administradora"
+let ehTextooo = typeof(profissao) == "string"
+
+let idade = 33
+let ehNumero = typeof(idade) == "string"
+
+let lugar = "japao"
+let ehTextoooo = typeof (lugar) == "string"
+
+
+console.log(ehTexto)
+console.log(ehTextoo)
+console.log(ehTextooo)
+console.log(ehNumero)
+console.log(ehTextoooo)
+
+
+
+
+
+
