7a15476864fe6b9e20749ca34fe1260853db5949
celiceceron <celice.cer@gmail.com>
2022-10-09 23:50:31 +0200

    utilizando template strings

--- tarefa-semana-1/pratica-5.js
+++ tarefa-semana-1/pratica-5.js
@@ -1,5 +1,15 @@
-const nome = "celice"
+let nome = "celice"
 const email = "celice.cer@gmail.com"
 let profissao = "administradora"
 let idade = 33
-let lugar = "japao"
+let lugar = "japão"
+
+
+
+// texto Olá, <valor da variável que tem o seu nome>! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para <valor da variável que armazena o lugar que você sonha conhecer>. Confirma pra gente alguns dados? A sua idade é <valor da variável que armazena sua idade>, você é <valor da variável que armazena sua profissão> e o seu email é <valor da variável que armazena seu email>? Ficamos no aguardo. Muito obrigado e boa viagem! :D
+
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreass para ${lugar}.
+Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu 
+email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+
