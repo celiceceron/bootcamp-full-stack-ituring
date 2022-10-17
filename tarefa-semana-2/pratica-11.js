2a15459d1a0cd12f395dbf22ba450aa4ee2f331d
celiceceron <celice.cer@gmail.com>
2022-10-17 17:41:22 +0200

    identificando numero maior com if e else

---
+++ tarefa-semana-2/pratica-11.js
@@ -0,0 +1,23 @@
+/* 
+Criar uma função que recebe uma cor escrita em texto 
+como parâmetro e retorna o valor no formato RGB.
+
+vermelha"->"rgb(255,0,0)"
+"laranja"->"rgb(255,128,0)"
+"amarela"->"rgb(255,255,0)"
+"verde"->"rgb(,255,)"
+"azul claro"->"rgb(0,255,255)"
+"azul marinho"->"rgb(0,0,255)"
+"rosa"->"rgb(255,0,255)"
+ "rgb(255,255,255)".
+
+*/
+
+function definicaoDeCores (texto) {
+    switch (definicaoDeCores) {
+        case (texto === vermelha):
+            console.log(`vermelha"->"rgb(255,0,0)"`);
+        break;
+    }
+}
+console.log(definicaoDeCores(vermelha));
---
+++ tarefa-semana-2/pratica-9.js
@@ -0,0 +1,19 @@
+/*
+Função recebe dois parâmetros (dois números) e o valor 
+exibido na tela deverá ser o de maior valor. 
+Por exemplo, passando 10 e 20 como parâmetros, 
+o número 20 deverá ser o resultado obtido. 
+Já passando 2 números iguais, você pode exibir
+qualquer um dos números.
+*/
+
+
+function maiorNumero (num1, num2){
+    if(num1 < num2){
+        console.log(num2);
+    } else{
+        console.log(num1, num2);
+    }
+}
+
+console.log(maiorNumero(10, 10));
