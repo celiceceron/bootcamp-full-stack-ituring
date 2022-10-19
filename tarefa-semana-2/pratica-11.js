5ece3863c4df7cdfb6992a648fe55f9425a267a6
celiceceron <celice.cer@gmail.com>
2022-10-19 02:35:18 +0200

    codando com switch

--- tarefa-semana-2/pratica-11.js
+++ tarefa-semana-2/pratica-11.js
@@ -16,28 +16,28 @@ vermelha"->"rgb(255,0,0)"
 const identificaAsCores = (cor) => {
     switch (cor) {
         case "vermelha":
-            console.log(`vermelha"->"rgb(255,0,0)"`);
+            return("rgb(255,0,0)");
             break;
         case "laranja":
-            console.log(`"laranja"->"rgb(255,128,0)"`);
+            return "rgb(255,128,0)";
             break;
         case "amarela":
-            console.log(`"verde"->"rgb(,255,)"`);
+            return "rgb(,255,)";
             break;
         case "verde":
-            console.log(`"verde"->"rgb(,255,)"`);
+            return "rgb(,255,)";
             break;
         case "azul claro":
-            console.log(`"azul claro"->"rgb(0,255,255)"`);
+            return "rgb(0,255,255)";
             break;
         case "azul marinho":
-            console.log(`"azul marinho"->"rgb(0,0,255)"`);
+            return "rgb(0,0,255)";
             break;
         case "rosa":
-            console.log(`"rosa"->"rgb(255,0,255)"`);
+            return "rgb(255,0,255)";
             break;
         default:
-            console.log(`"rgb(255,255,255)".`);
+            return "rgb(255,255,255)";
     }
 }
 console.log(identificaAsCores("vermelha"));
