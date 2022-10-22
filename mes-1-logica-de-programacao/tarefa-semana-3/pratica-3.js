7d73f2ebb287b99edf39b6f647b174b6cc68acdc
celiceceron <celice.cer@gmail.com>
2022-10-22 22:58:34 +0200

    numeros primos com while

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-3.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-3.js
@@ -2,31 +2,51 @@
 Crie uma função que recebe como argumento um número 
 inteiro maior que zero, retorne true se este número 
 for primo e false se ele não for um número primo.
+*/
 
-
+/*
 // NUMERO PRIMO TEM 2 DIVISORES/ NUMERO 1 E ELE MESMO.
-const eHPrimo = (n) => {
-    let i = 2;
+function numeroPrimo (n) {
 
-
-    while (i <= n) {
-
-       if (n % i === 0){
-        console.log(i);
-            return true;
-       }
-       i++;
+    for (let i = 2; i < n; i++) {
+        if(n % i === 0){
+            return false;
+            break;
+        }
     }
-
-return false;
+        return true;
 }
 
-eHPrimo(17);
-eHPrimo(2);
-eHPrimo(7);
-eHPrimo(8);
-eHPrimo(5);
-eHPrimo(23);
-eHPrimo(15);
-eHPrimo(1500450271);
+console.log(numeroPrimo(17));
+console.log(numeroPrimo(2));
+console.log(numeroPrimo(7));
+console.log(numeroPrimo(8));
+console.log(numeroPrimo(5));
+console.log(numeroPrimo(23));
+console.log(numeroPrimo(15));
+console.log(numeroPrimo(1500450271));
 */
+
+// TENTATIVA 2:
+
+function numeroPrimo (n) {
+    let i = 2;
+
+    while (i < n) {
+        if(n % i === 0) {
+            return false;
+        }
+        i++;
+    }
+        return true;
+}
+
+console.log(numeroPrimo(17));
+console.log(numeroPrimo(2));
+console.log(numeroPrimo(7));
+console.log(numeroPrimo(8));
+console.log(numeroPrimo(5));
+console.log(numeroPrimo(23));
+console.log(numeroPrimo(15));
+console.log(numeroPrimo(1500450271));
+
