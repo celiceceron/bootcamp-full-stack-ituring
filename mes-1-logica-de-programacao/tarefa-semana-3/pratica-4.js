7dbf30dbe3b424a28d7d7e8cd3491192d91f73d6
celiceceron <celice.cer@gmail.com>
2022-10-22 23:37:07 +0200

    exibindo lista

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
@@ -1,13 +1,17 @@
 /*
 Crie uma função que recebendo como argumento uma lista 
-de seriados quaiquer, exiba na tela cada um dos seriados 
+de seriados qualquer, exiba na tela cada um dos seriados 
 com o índice de cada um na lista. 
 */
 
-const listaDeSeries = (lista) =>{
-    const lista = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];
 
-    let
+const geradorDeLista = (lista) => {
 
+    for (let [indice, item] of lista.entries())
+        console.log(`[${indice}] ${item}`);
 
 }
+
+let lista = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];
+
+geradorDeLista(lista);
