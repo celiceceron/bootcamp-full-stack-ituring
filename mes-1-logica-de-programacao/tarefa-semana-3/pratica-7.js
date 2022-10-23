770f31729e38d7b9ea3cefd017bc27eb04110a6d
celiceceron <celice.cer@gmail.com>
2022-10-23 22:35:57 +0200

    codando comm reduce

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-7.js
@@ -4,18 +4,10 @@ de depósitos e retiradas, exiba o saldo final da
 conta via console.log() dentro da função.
 */
 
-
-function somarTudo(listaDeEntradasESaidas) {
-let i = 0;
-let soma = [];
-
-    for(let i = 0; i < listaDeEntradasESaidas.length; i++) {
-    soma = soma + listaDeEntradasESaidas[i]
-        console.log(`O saldo final de sua conta foi positivo no valor de R$ ${soma}`)
-        return soma;
-    }
-
-}
-    
 const listaDeEntradasESaidas = [100, -20, -30, 10, -7, -21, -5];
-somarTudo(listaDeEntradasESaidas);
+
+const total = listaDeEntradasESaidas.reduce((acumulador, valorAtual) => {
+    return acumulador + valorAtual}, 0);
+
+console.log(`O saldo final de sua conta foi positivo no valor de R$ ${total}.`);
+
