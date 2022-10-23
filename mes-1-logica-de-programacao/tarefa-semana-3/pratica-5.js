f8d9ef3f4f188ca66147df7332bd1eaa5f9f9a17
celiceceron <celice.cer@gmail.com>
2022-10-23 02:56:37 +0200

    testando as listas

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
@@ -5,3 +5,23 @@ retorne a posição que se encontra o seriado.
 Caso não tenha encontrado o seriado, retorne undefined. 
 */
 
+/*
+const encontraSerie = (listaDeSeriados, buscaDeSerie) => {
+    let posicaoDaBusca = 0;
+
+    for (let i = 0; i < listaDeSeriados.length; i++) {
+        if (listaDeSeriados[i].includes(buscaDeSerie)){
+            posicaoDaBusca.push(listaDeSeriados[i])
+        }
+        return posicaoDaBusca
+
+        }
+
+        
+    }
+
+const listaDeSeriados = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];
+const buscaDeSerie = "Dexter";
+
+console.log(encontraSerie(listaDeSeriados, buscaDeSerie));
+*/
