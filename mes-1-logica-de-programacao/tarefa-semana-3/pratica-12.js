7b575080c1063cdf0b3d337fdc644ebaaab3482c
celiceceron <celice.cer@gmail.com>
2022-10-25 01:03:58 +0200

    criando grupos de arrays comm slice

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-12.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-12.js
@@ -4,3 +4,19 @@ pessoas e exibe na tela os grupos formados com 2 pessoas.
 Utilize o console.log() dentro da função para exibir os grupos. 
 Se faltar alguém, acrescente-a no último grupo. 
 */
+
+
+function dividirEmGrupos(alunos, tamanho) {
+
+    let novosGrupos = [];
+    let i = 0;
+
+    while (i < alunos.length) {
+        novosGrupos.push(alunos.slice(i, i + tamanho));
+        i += tamanho;
+}
+    return novosGrupos;
+   
+}
+
+console.log(dividirEmGrupos(["Anderson Luz", "Fabio Vechia", "Claudia Lopes", "Rafael Alves", "Maria Mansano", "Jairo Ferreira", "Vitor Ribeiro"], 2));
