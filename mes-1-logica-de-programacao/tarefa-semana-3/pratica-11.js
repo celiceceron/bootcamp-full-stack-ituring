5bcb0a857bbae275cc004a5bf0757b49486ad718
celiceceron <celice.cer@gmail.com>
2022-10-24 23:08:48 +0200

    ordenando array com o metodo sort

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-11.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-11.js
@@ -4,4 +4,12 @@ de números que representam a contagem de visualizações
 de vídeos de um canal no Youtube. Retorne a lista com os 
 números ordem crescente. Tente escrever um algoritmo de ordenação 
 para resolver este problema. 
-/*
+*/
+
+const listaDeNumeros = [10, 45, 123, 18, 56, 45, 32, 98, 121, 232, 1392824];
+
+const ordemCrescente = listaDeNumeros.sort (function(a, b){
+    return a - b;
+});
+
+console.log(ordemCrescente);
--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -5,7 +5,7 @@ conta com as seguintes informações no exemplo abaixo.
 Para exibir, utilize o console.log() dentro da função.
 */
 
-
+/*
 const listaDeEntradasESaidas = [100, -20, -30, 10, -7, -21, -5];
 const valoresDeentrada = [[0], [3]];
 const valoresDeSaida = [[1], [2], [4], [5], [6]];
@@ -16,3 +16,4 @@ const total = listaDeEntradasESaidas.reduce((acumulador, valorAtual) => {
     return acumulador + valorAtual}, 0);
 
 console.log(`O saldo final de sua conta foi positivo no valor de R$ ${total}.`);
+*/
