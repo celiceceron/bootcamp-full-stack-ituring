5839069cfc44fea771a65707b202471068a001dd
celiceceron <celice.cer@gmail.com>
2022-10-24 22:24:45 +0200

    ordenando a lista com sort

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-10.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-10.js
@@ -4,3 +4,76 @@ de votos, exiba na tela quantos votos recebeu cada país
 com base no código acima. 
 Para exibir, utilize o console.log() dentro da função.
 */
+
+
+    const votacaoPaises = [
+    {
+        Pais: "Brasil",
+    votos: 3
+    },
+    {
+        Pais: "Argentina", 
+    votos: 2
+    },
+    {
+        Pais: "Espanha", 
+    votos: 1
+    },
+    {
+        Pais: "Portugal", 
+    votos: 2
+    }
+    ];
+    
+    votacaoPaises.sort(function(a, b) {
+        if(a.Pais < b.votos) {
+            return -1;
+        } else {
+            return true;
+        }
+        
+    });
+
+    console.log(votacaoPaises);
+
+    /*
+    Brasil: 3 voto(s)
+    Argentina: 2 voto(s)
+    Espanha: 1 voto(s)
+    Portugal: 2 voto(s)
+    
+    Resultado:
+    Brasil é o preferido para ganhar a Copa do Mundo!
+    /*
+
+    // EX:
+    /*
+const pessoas = [
+    {
+        nome: "Matheus", 
+        idade: 29
+    },
+    {
+        nome: "Pedro", 
+        idade: 18
+    },
+    {
+        nome: "Maria,",
+        idade: 40
+    },
+    {
+        nome: "Alan",
+        idade: 63
+    }
+];
+
+pessoas.sort(function(a, b) {
+    if(a.nome < b.nome) {
+        return -1;
+    } else {
+        return true;
+    }
+});
+
+console.log(pessoas);
+*/
--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -5,10 +5,11 @@ conta com as seguintes informações no exemplo abaixo.
 Para exibir, utilize o console.log() dentro da função.
 */
 
+
 const listaDeEntradasESaidas = [100, -20, -30, 10, -7, -21, -5];
 const valoresDeentrada = [[0], [3]];
 const valoresDeSaida = [[1], [2], [4], [5], [6]];
-const saldoFinal = (valoresDeentrada + valoresDeSaida);
+const saldoFinal = (valoresDeentrada - valoresDeSaida);
 
 
 const total = listaDeEntradasESaidas.reduce((acumulador, valorAtual) => {
