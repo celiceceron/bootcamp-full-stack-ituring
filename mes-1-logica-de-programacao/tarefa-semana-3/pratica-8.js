3b169920445d957ace1228fb16a2f7d500b93120
celiceceron <celice.cer@gmail.com>
2022-10-25 17:39:28 +0200

    calculando um extrato

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -5,11 +5,30 @@ conta com as seguintes informações no exemplo abaixo.
 Para exibir, utilize o console.log() dentro da função.
 */
 
+
+    const listaDeEntradasESaidas = [100, -20, -30, 10, -7, -21, -5];
+    const negativos = ([[1] + [2] + [4] + [5] + [6]]);
+    const totalNegativos = 0;
+    
+    const listaDeSaidas = (listaDeEntradasESaidas) => {
+    
+    for (let i = 0; i > listaDeEntradasESaidas.length; i++)
+        return totalNegativos
+      }
+     
+    
+
 /*
-const listaDeEntradasESaidas = [100, -20, -30, 10, -7, -21, -5];
-const valoresDeentrada = [[0], [3]];
-const valoresDeSaida = [[1], [2], [4], [5], [6]];
-const saldoFinal = (valoresDeentrada - valoresDeSaida);
+const listaDeSaidas = (negativo) => {
+    for();
+    console.log();
+}
+
+const resultadoDoExtrato = (positivoMenosNegativo) => {
+    for();
+    console.log();
+
+}
 
 
 const total = listaDeEntradasESaidas.reduce((acumulador, valorAtual) => {
@@ -17,3 +36,6 @@ const total = listaDeEntradasESaidas.reduce((acumulador, valorAtual) => {
 
 console.log(`O saldo final de sua conta foi positivo no valor de R$ ${total}.`);
 */
+
+console.log(listaDeEntradasESaidas);
+
