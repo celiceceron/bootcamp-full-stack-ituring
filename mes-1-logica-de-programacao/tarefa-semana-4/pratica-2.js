a8a8132bd95fee25fdd75d5a9c937122a75839b4
celiceceron <celice.cer@gmail.com>
2022-10-28 22:41:47 +0200

    criando funcao usando forEach

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-2.js
@@ -0,0 +1,22 @@
+/*
+Crie uma função que recebendo como argumento uma lista 
+de seriados e o seriado que esteja querendo pesquisar, 
+retorne a posição que se encontra o seriado. 
+Caso não tenha encontrado o seriado, retorne undefined. 
+*/
+
+function encontraSerie(series, termodebusca) {
+    let indice = undefined
+    series.forEach((item, i) => {
+        if(item === termodebusca){
+            indice = i
+        }
+    });
+        console.log([indice]);
+}
+
+  
+        
+
+let listaSeries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+encontraSerie(listaSeries, "Dexter")
