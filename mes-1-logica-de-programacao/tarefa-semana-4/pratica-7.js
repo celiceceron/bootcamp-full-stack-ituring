28f88d726ea363a71426f683646aa5f820b6d33f
celiceceron <celice.cer@gmail.com>
2022-10-31 23:13:15 +0100

    criando funcoes com Object.keys

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-7.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-7.js
@@ -4,27 +4,21 @@ e imprima a lista de chaves que ele possui.
 */
 
 
-const exibeFilme = (filme) => {
-    console.table("O filme ");
-    console.table(filme.titulo);
-    console.table("que estreou em ");
-    console.table(filme.anoPublicacao);
-    console.table("tem a avaliação de ");
-    console.table(filme.notaAvaliacao);
-    console.table("pelo IMDb.");
+const exibeinformacoes = (cozinha) => {
+    console.log(Object.keys(c));
 };
 
-let f = {
-    titulo: "Estrelas Além do Tempo",
-    notaAvaliacao: 7.8,
-    duracao: 127,
-    anoPublicacao: 2016,
-    categoria: "Drama"
+let c = {
+    marca: "Tramontina",
+    produto: "Jogo de talheres",
+    preco: 500,
+    quantidade: 132,
+    categoria: "Utensilhos domésticos"
 
 };
 
-exibeFilme(f);
+exibeinformacoes(c);
 
-// array com objeto
-var obj = { 0: 'a', 1: 'b', 2: 'c' };
+/*
 console.log(Object.keys(obj)); // console: ['0', '1', '2']
+*/
