0ab85231be32e5c1aacd388ed1272e365d9b64e4
celiceceron <celice.cer@gmail.com>
2022-11-06 22:42:45 +0100

    ordenando a lista dentro do objeto

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-13.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-13.js
@@ -4,9 +4,22 @@ movimentações bancárias começando com a data
 mais recente até a data de movimentação mais antiga.
 */
 
-/*
-let l = 
-[{
+
+const ordenaData = (lista) => {
+
+    lista.forEach(function(item) {
+            return item.dataMovimentacao = new Date(item.dataMovimentacao)        
+    })
+
+    lista.sort(function(a, b) {
+        return b.dataMovimentacao - a.dataMovimentacao
+    })
+
+    console.log(lista)
+}
+
+
+let lista = [{
     valor: 100.00,
     movimentacao: "deposito",
     dataMovimentacao: "2021-08-02T07:46:36.611Z"
@@ -42,18 +55,6 @@ let l =
     dataMovimentacao: "2022-06-08T09:46:36.611Z"
   }]
 
-  
-  const ordenaData = (lista) => {
-    let ajusteDeData = new Date("2022-06-08T09:46:36.611Z", "2022-04-20T09:46:36.611Z", "2022-03-20T09:46:36.611Z", "2022-03-02T09:46:36.611Z", "2022-02-24T09:46:36.611Z", "2021-09-17T09:46:36.611Z", "2021-08-02T07:46:36.611Z");
+  ordenaData(lista)
 
-    lista.sort(function(a, b){
-        if(a < b) return -1;
-        if(a > b) return 1;
-        return 0;
- 
-    })
 
-        
-  };
-
-  ordenaData(l);
