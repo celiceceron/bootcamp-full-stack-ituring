7eae1cf3170668b963fb29e08de4400adca63894
celiceceron <celice.cer@gmail.com>
2022-11-08 22:58:28 +0100

    acrescentando novas opcoes de codigos

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-14.js
@@ -49,4 +49,57 @@ let estabelecimento = {
   }
 
  mediaNota(estabelecimento);
+  /*
+  //SOLUÇÃO 1 
+let exibeMediaAvaliacoesS1 = dadosRestaurante => {
+  let somaAvaliacoes = 0;
+  dadosRestaurante.avaliacoes.forEach((avaliacao) => {
+      somaAvaliacoes += avaliacao.nota;
+  });
+  const mediaAvaliacoes = somaAvaliacoes/dadosRestaurante.avaliacoes.length;
   
+  console.log(`A média de avaliações do restaurante ${dadosRestaurante.restaurante} foi de ${mediaAvaliacoes.toFixed(2)}.`);
+}
+
+//SOLUÇÃO 2 (Usando o método .reduce())
+O método .reduce() pega uma lista e reduz ela em um único valor. A função de callback desse método tem como 1º parâmetro o acumulador e o segundo parâmetro o valorAtual da lista. Além da função de callback, o método .reduce() também possui como parâmetro o valor inicial que você deseja colocar na variável acumuladora
+
+let exibeMediaAvaliacoesS2 = dadosRestaurante => {
+  const valorInicial = 0;
+  const funcaoRedutora = (acumulador, avaliacaoAtual) => acumulador += avaliacaoAtual.nota;
+  const somaAvaliacoes = dadosRestaurante.avaliacoes.reduce(funcaoRedutora, valorInicial);
+  const mediaAvaliacoes = somaAvaliacoes/dadosRestaurante.avaliacoes.length;
+  
+  console.log(`A média de avaliações do restaurante ${dadosRestaurante.restaurante} foi de ${mediaAvaliacoes.toFixed(2)}.`);
+}
+
+//TESTANDO AS SOLUÇÕES 
+const dadosRestaurante = {
+  restaurante: "Turing Gourmet",
+  tipo: "Comida britânica",
+  avaliacoes: [
+    {
+      cliente: "Thais S.",
+      nota: 9.8
+    },
+    {
+      cliente: "Thales Gonçalves",
+      nota: 8.9
+    },
+    {
+      cliente: "José Lopes",
+      nota: 9.9
+    },
+    {
+      cliente: "Cristina Souza",
+      nota: 9.3
+    },
+    {
+      cliente: "Leo Garcia",
+      nota: 8.5
+    }
+  ]
+};
+exibeMediaAvaliacoesS1(dadosRestaurante);
+exibeMediaAvaliacoesS2(dadosRestaurante);
+*/
--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-15.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-15.js
@@ -54,3 +54,52 @@ até o cliente (em minutos).
     }]
 
 encontraMotorista(l);
+
+/*
+//SOLUÇÃO 
+let exibeCarroACaminho = listaVeiculos => {
+  const veiculoMaisProximo = listaVeiculos.sort((veiculoAnterior, veiculoAtual) => 
+      veiculoAnterior.tempoEstimado - veiculoAtual.tempoEstimado
+  )[0];
+
+  console.log(`${veiculoMaisProximo.motorista} está a caminho com o carro Fox de placa ${veiculoMaisProximo.placa}. Chega em ${veiculoMaisProximo.tempoEstimado} minutos.`);
+}
+
+//TESTANDO A SOLUÇÃO 
+const listaVeiculos = [{
+  motorista: "Thais S.",
+  avaliacao: 9.8,
+  tempoEstimado: 7,
+  veiculo: "Palio",
+  placa: "ABC4321"
+},
+{
+  motorista: "Thales Gonçalves",
+  avaliacao: 8.9,
+  tempoEstimado: 4,
+  veiculo: "Celta",
+  placa: "ABC0987"
+},
+{
+  motorista: "José Lopes",
+  avaliacao: 9.9,
+  tempoEstimado: 5,
+  veiculo: "Fox",
+  placa: "ABC1234"
+},
+{
+  motorista: "Cristina Souza",
+  avaliacao: 9.3,
+  tempoEstimado: 3,
+  veiculo: "Fox",
+  placa: "ABC1234"
+},
+{
+  motorista: "Leo Garcia",
+  avaliacao: 8.5,
+  tempoEstimado: 9,
+  veiculo: "Ka",
+  placa: "ABC8765"
+}]
+exibeCarroACaminho(listaVeiculos);
+*/
